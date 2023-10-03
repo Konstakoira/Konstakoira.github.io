4872ac4597a61bb580f9cee296f55e44563bfac6
Konsta Perttilä <konsta.perttila@hotmail.com>
2023-10-03 03:21:30 +0300

    testing

---
+++ src/Frontpage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FrontPage = () => {
+    return (
+        <div className="firstView">
+            <h1>Kissatkoiria</h1>
+        </div>
+    );
+};
+
+export default FrontPage;
